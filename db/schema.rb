0ab2161960aad38a91e536afd2f66e614ab301c3
Huyuhotaru <108530430+Huyuhortaru@users.noreply.github.com>
2022-10-04 15:13:13 +0000

    [Tenth]commit

--- db/schema.rb
+++ db/schema.rb
@@ -48,11 +48,6 @@ ActiveRecord::Schema.define(version: 2022_08_28_114951) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_images", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_08_28_114951) do
     t.datetime "remember_created_at"
     t.string "name"
     t.text "introduction"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
